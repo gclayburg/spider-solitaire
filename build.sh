71e55dbffab7b437967ce762c3dd9f58406e689b
Gary Clayburg <gclaybur@comcast.net>
2016-03-03 10:51:45 -0700

    added ability to run as applet

--- build.sh
+++ build.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-mvn clean install && cp -rp spiderapplet/target/jnlp/* .
+mvn clean install && cp -rp spiderapplet/target/jnlp/* . && cp -rp spiderapplet/src/main/resources/spider.html lib/
+
+# run app via webstart:
+# javaws spider.jnlp
+
+# run app via java:
+# java -jar lib/spiderapplet-1.0-SNAPSHOT.jar
+
+# run app via appletviewer:
+# appletviewer lib/spider.html
+
